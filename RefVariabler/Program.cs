e139e382e90d646790c580120416def7e09667e2
Michell <devcronberg@gmail.com>
2018-05-23 13:15:01 +0200

    refvar færdig

--- RefVariabler/Program.cs
+++ RefVariabler/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Csv;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace RefVariabler
 {
@@ -34,7 +37,7 @@ namespace RefVariabler
             Console.WriteLine(a3.Length);
 
             int[] a5 = { 1, 2, 7, 1, 4, 4, 3, 47 };
-            a5 = null;
+            //a5 = null;
 
             for (int i = 0; i < a5.Length; i++)
             {
@@ -46,7 +49,107 @@ namespace RefVariabler
                 Console.WriteLine(i);
             }
 
+            char t = 'A';
+            string navn = "Mikkel";
+            navn = navn.ToUpper();
+
+            //string sti = "c:\temp\test.txt";
+            //string sti = "c:\\temp\\test.txt";
+            string sti = @"c:\temp\test.txt";
+
+            string text = "dlfkjæsdlkfjg\r\ndgkdhgsdkjhg";
+            Console.WriteLine(text);
+
+            // csv assembly
+            string csv2 = "324;223;234;2132;23";
+            string[] aa = csv2.Split(';');
+
+            string b = "dsfæfdk" + " " + text + "*";
+            int alder = 10;
+            string navn2 = "Mikkel";
+
+            string c = $"Personen {navn2.ToUpper()} er {alder-1} år.";
+
+            Console.WriteLine("Start");
+            System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+            s.Start();
+            string tmp = "";
+            for (int i = 0; i < 100_000; i++)
+            {
+                tmp += "*";
+            }
+            s.Stop();
+            Console.WriteLine($"Tid = {s.ElapsedMilliseconds}");
+
+            System.Text.StringBuilder sb 
+                = new System.Text.StringBuilder();
+            s.Reset();
+            s.Start();            
+            for (int i = 0; i < 10000000; i++)
+            {
+                sb.Append("*");
+            }
+            s.Stop();
+            Console.WriteLine($"Tid = {s.ElapsedMilliseconds}");
+
+            var csv = File.ReadAllText("data.csv");
+            foreach (var line in CsvReader.ReadFromText(csv))
+            {
+                // Header is handled, each line will contain the actual row data
+                var firstCell = line[0];
+                var byName = line["b"];
+            }
+
+
+            int y = 1;
+            switch (y)
+            {
+                case 1:
+                case 2:
+                    // kode
+
+
+                    break;
+                case 3:
+                    break;
+
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 10; i >0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 10; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;
+
+                Console.WriteLine(i);
+                if (i == 7)
+                    break;
+            }
 
         }
+
+      
     }
+
+
+
+    
+
+ 
 }
