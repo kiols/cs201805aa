9ed945e6ab53a3e1340626d66d88235a812facbc
Michell Cronberg <admin@AR-114-INS>
2018-06-19 13:24:33 +0200

    di

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ITilfældighedsGenerator r = new TilfældighedsGeneratorMock();
+            ITilfældighedsGenerator r = TilfældighedsGeneratorHjælper.FindGenerator();
+            Terning t = new Terning(r);
+            Console.WriteLine(t);
+
+        }
+    }
+
+    class Terning
+    {
+        public int Værdi { get; private set; }
+        private ITilfældighedsGenerator rnd;
+
+        public Terning(ITilfældighedsGenerator i)
+        {
+            this.rnd = i;
+            Ryst();
+        }
+
+        public Terning(int værdi)
+        {
+            if (værdi < 1 || værdi > 6)
+                throw new ApplicationException("Forkert værdi på terning");
+            this.Værdi = værdi;
+        }
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+
+        }
+
+        public override string ToString()
+        {
+            return "[" + this.Værdi + "]";
+        }
+    }
+
+    interface ITilfældighedsGenerator
+    {
+        int Next(int min, int max);
+    }
+
+    static class TilfældighedsGeneratorHjælper
+    {
+        public static ITilfældighedsGenerator FindGenerator()
+        {
+            //if(DateTime.Now.Millisecond%2==0)
+            //    return new TilfældighedsGeneratorMock();
+            //else
+            //    return new TilfældighedsGeneratorSystemRandom();
+
+            string v = System.Configuration.ConfigurationManager.AppSettings["generator"];
+
+            if (v == "mock")
+                return new TilfældighedsGeneratorMock();
+            else if (v == "random")
+                return new TilfældighedsGeneratorSystemRandom();
+            else
+                throw new ApplicationException("Forkert generator");
+        }
+
+
+    }
+
+    class TilfældighedsGeneratorSystemRandom : ITilfældighedsGenerator
+    {
+        private static System.Random rnd = new Random();
+
+        public int Next(int min, int max)
+        {
+            return rnd.Next(min, max);
+        }
+    }
+
+    class TilfældighedsGeneratorMock : ITilfældighedsGenerator
+    {
+
+
+        public int Next(int min, int max)
+        {
+            return 6;
+        }
+    }
+
+    // Gammel terning
+    class Terning2
+    {
+        public int Værdi { get; private set; }
+        // hmmmmm - vi bundet System.Random
+        private static System.Random rnd = new Random();
+
+        public Terning2()
+        {
+            Ryst();
+        }
+
+        public Terning2(int værdi)
+        {
+            if (værdi < 1 || værdi > 6)
+                throw new ApplicationException("Forkert værdi på terning");
+            this.Værdi = værdi;
+        }
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+
+        }
+
+        public override string ToString()
+        {
+            return "[" + this.Værdi + "]";
+        }
+    }
+}
---
+++ using/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace usingdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            //Bil b = new Bil();
+            //try
+            //{
+
+            //}
+            //catch (Exception)
+            //{
+
+            //}
+            //finally
+            //{
+            //    b.Dispose();
+            //}
+
+            using (Bil b = new Bil())
+            {
+                //b.gem
+            } // dispose bliver kaldt, og den 
+              // skal sørge for at lukke og rydde op...
+
+
+
+            Console.WriteLine("Slut");
+
+
+            Bil t = new Bil();
+            t.Dispose();
+
+
+            Console.WriteLine();
+
+            //System.Data.SqlClient.SqlConnection cn = new System.Data.SqlClient.SqlConnection("...");
+            //cn.Open();
+            //cn.Dispose();
+
+            using (System.Data.SqlClient.SqlConnection cn = new System.Data.SqlClient.SqlConnection("..."))
+            {
+                cn.Open();
+                using (System.Data.SqlClient.SqlDataAdapter da = new System.Data.SqlClient.SqlDataAdapter("select * ", cn))
+                {
+                    
+
+                } // dispose
+            }
+
+            //using (System.Random rnd = new Random())
+            //{
+
+            //}
+
+            var a = System.IO.File.ReadAllLines(@"c:\temp\data.csv");
+
+
+
+        }
+    }
+
+    class Bil : IDisposable
+    {
+
+        public Bil()
+        {
+            Console.WriteLine("Jeg fødes");
+        }
+
+        //~Bil()
+        //{
+        //    // io, db, tcp
+        //    Console.WriteLine("Jeg dør");
+        //}
+
+        public void Dispose()
+        {
+            Console.WriteLine("Rydder op - og så må jeg dø");
+        }
+    }
+}
